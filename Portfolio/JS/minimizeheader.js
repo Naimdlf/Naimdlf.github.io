7d670d8b78dcfd4e1e663c2cb4ae28828148d83e
Naimdlf <Naimdlf@icloud.com>
2025-04-14 21:05:22 -0700

    Add files via upload

---
+++ Portfolio/JS/minimizeheader.js
@@ -0,0 +1,25 @@
+const header = document.querySelector("header");
+const title = document.querySelector("h1");
+const buttons = document.querySelectorAll("button");
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    header.style.padding = "1px 5px";
+    header.style.borderTopLeftRadius = "0";
+    header.style.borderTopRightRadius = "0";
+    header.style.transform = "scaleX(0.96)";
+    title.style.fontSize = "16px";
+    buttons.forEach(btn => {
+      btn.style.padding = "1px 5px";
+    });
+  } else {
+    header.style.padding = "10px 15px";
+    header.style.borderTopLeftRadius = "8px";
+    header.style.borderTopRightRadius = "8px";
+    header.style.transform = "scaleX(1)";
+    title.style.fontSize = "24px";
+    buttons.forEach(btn => {
+      btn.style.padding = "10px 10px";
+    });
+  }
+};
