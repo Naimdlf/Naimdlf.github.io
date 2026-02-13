a419e43c6197c71c0a8359c08f97a2e3d0233595
Naimdlf <Naimdlf@icloud.com>
2026-02-13 10:48:24 -0800

    try 2

---
+++ portfolio/webmaps/final/sidebar/panel.js
@@ -0,0 +1,24 @@
+
+
+const sidebar = document.getElementById("mySidebar");
+const toggleBtn = document.getElementById("sidebarbtn");
+const sidebarContent = document.getElementById("sidebarContent");
+
+// button js
+
+toggleBtn.onclick = () => {
+  // Check current width of the sidebar
+  if (sidebar.style.width === "250px") {
+    // If open, close it
+    sidebar.style.width = "0";
+    toggleBtn.innerHTML = "☰ Sidebar";
+  } else {
+    // If closed, open it
+    sidebar.style.width = "250px";
+    toggleBtn.innerHTML = "× Close";
+  }
+
+}
+
+
+
