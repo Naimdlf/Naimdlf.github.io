188c43e94a9c47e04c6f5bd891978a3473cd4aea
Naimdlf <Naimdlf@icloud.com>
2026-02-17 10:07:26 -0800

    Update config.js

--- webmaps/final/config.js
+++ webmaps/final/config.js
@@ -1,2 +1,2 @@
-const MAPBOX_TOKEN = 'pk.eyJ1IjoibmZlcnJpczEiLCJhIjoiY21scWNnMW9uMW9qOTNkcHVzc3c2Y2lsYiJ9.-YetL6y1I76WFhsGyKkHLA'; 
+const MAPBOX_TOKEN = 'pk.eyJ1IjoibmZlcnJpczEiLCJhIjoiY21scTZnenBtMHVqODNlcTZsZGF4a2ZiNyJ9.7UGrFE1U37aXzTuzuyY0wQ'; 
 // TokenMapbox
