0aaf06414c8b4987298625eac27104babd414d3e
Naimdlf <Naimdlf@icloud.com>
2026-02-17 10:54:01 -0800

    h

--- webmaps/final/panel.js
+++ webmaps/final/panel.js
@@ -1,7 +1,9 @@
 const sidebar = document.getElementById("mySidebar");
 const toggleBtn = document.getElementById("toggleSidebarBtn");
+const sidebarContent = document.getElementById("sidebarContent");
 
 toggleBtn.onclick = () => {
+  const currentWidth = window.getComputedStyle(sidebar).width;
   if (sidebar.style.width === "250px") {
     // close
     sidebar.style.width = "0";
