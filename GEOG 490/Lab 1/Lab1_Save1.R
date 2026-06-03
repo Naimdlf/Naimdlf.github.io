cf98be7bf4c0935903d1b3dab9b960963737ff37
Naimdlf <Naimdlf@icloud.com>
2026-06-03 14:21:25 -0700

    port

--- GEOG 490/Lab 1/Lab1_Save1.R
+++
@@ -1,2 +0,0 @@
-library(tidycensus)
-census_api_key("fbb3f7914f7097828de7fd0ce50ab55c51222d60", install = TRUE)
---
+++ GEOG_490/Lab 1/Lab1_Save1.R
@@ -0,0 +1,2 @@
+library(tidycensus)
+census_api_key("fbb3f7914f7097828de7fd0ce50ab55c51222d60", install = TRUE)
